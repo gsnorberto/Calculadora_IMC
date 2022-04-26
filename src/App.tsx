f72bc5d6b992c662417a85a99336e253120d0b36
Gabriel Norberto <sn_gabriel@outlook.com>
2022-04-26 12:53:39 -0300

    Criado o layout e estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import styles from './App.module.css'
-import poweredImage from './assets/powered.png'
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png';
+import { useState } from 'react';
+import { GridItem } from './components/GridItem';
+
+import { levels, calculateImc } from './helpers/imc'
 
 export default () => {
+
+    const [heightField, setHeightField] = useState<number>(0);
+    const [weightField, setWeightField] = useState<number>(0);
+
+    const handleCalculateButton = () => {
+        if (heightField && weightField) {
+
+        } else {
+            alert("Digite todos os campos")
+        }
+    }
+
     return (
         <div className={styles.main}>
             <header>
@@ -10,6 +26,36 @@ export default () => {
 
                 </div>
             </header>
+
+            <div className={styles.container}>
+                <div className={styles.leftSide}>
+                    <h1>Calcule seu IMC.</h1>
+                    <p>O índice de massa corporal é uma medida internacional usada para calcular se uma pessoa está no peso ideal.</p>
+
+                    <input
+                        type="number"
+                        placeholder="Digite a sua altura (em métros). Ex: 2.3"
+                        value={heightField > 0 ? heightField : ''}
+                        onChange={e => setHeightField(parseFloat(e.target.value))}
+                    />
+
+                    <input
+                        type="number"
+                        placeholder="Digite o seu peso (em kg). Ex: 70.3"
+                        value={weightField > 0 ? weightField : ''}
+                        onChange={e => setWeightField(parseFloat(e.target.value))}
+                    />
+
+                    <button onClick={handleCalculateButton}>Calcular</button>
+                </div>
+                <div className={styles.rightSide}>
+                    <div className={styles.grid}>
+                        {levels.map((item, key)=>(
+                            <GridItem key={key} item={item} /> 
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
